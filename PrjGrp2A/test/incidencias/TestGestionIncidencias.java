67a1c1878599c4c945e3a1c5c46f918a0316ffb9
AlbertoFontanUSC <alberto.fontan@outlook.es>
2021-05-14 01:01:57 +0200

    testIncidenciasInProgress

--- PrjGrp2A/test/incidencias/TestGestionIncidencias.java
+++ PrjGrp2A/test/incidencias/TestGestionIncidencias.java
@@ -6,7 +6,11 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import com.enso.ayuntamiento.Ciudadano;
+import com.enso.ayuntamiento.Concejal;
+
 import incidencias.TipoIncidencia;
+import procesos.GestionProcesos;
+import procesos.Proceso;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -119,7 +123,7 @@ class TestGestionIncidencias {
 		// Act
 		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
 		// Assert
-		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de un tipo no String");
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de menos de 9 caracteres");
 	}
 	
 	@DisplayName("CP04-Prueba1.1_buscarIncidencias no válido con parámetro dni incorrecto con 10 caracteres")
@@ -146,7 +150,7 @@ class TestGestionIncidencias {
 		// Act
 		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
 		// Assert
-		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de un tipo no String");
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de más de 9 caracteres");
 	}
 	
 	@DisplayName("CP05-Prueba1.1_buscarIncidencias no válido con parámetro idIncidencia incorrecto con 6 caracteres")
@@ -173,7 +177,7 @@ class TestGestionIncidencias {
 		// Act
 		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
 		// Assert
-		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de un tipo no String");
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro idIncidencia con más de 5 caracteres");
 	}
 	
 	@DisplayName("CP06-Prueba1.1_buscarIncidencias no válido con parámetro idIncidencia incorrecto con 4 caracteres")
@@ -183,7 +187,7 @@ class TestGestionIncidencias {
 		// Arrange
 		GestionIncidencias gi = new GestionIncidencias();
 		String dni = "12345678P";
-		String idIncidencia = "I1234";
+		String idIncidencia = "I1";
 		Date fechaInicio = new Date(2021 - 1900, 01, 01);
 		Date fechaFin = new Date(2021 - 1900, 12, 31);
 		TipoIncidencia tipo = TipoIncidencia.Desperfectos;
@@ -201,7 +205,143 @@ class TestGestionIncidencias {
 		// Act
 		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
 		// Assert
-		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro dni de un tipo no String");
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro idIncidencia incorrecto con 4 caracteres");
+	}
+	
+	@DisplayName("CP07-Prueba1.1_buscarIncidencias no válido con fechaInicio distintia de tipo Date")
+	@Test
+	void CP07_Prueba1_1_buscarIncidencias() {
+		// Arrange
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni = "12345678P";
+		String idIncidencia = "I1";
+		Date fechaInicio = new Date("mala");
+		Date fechaFin = new Date(2021 - 1900, 12, 31);
+		TipoIncidencia tipo = TipoIncidencia.Desperfectos;
+
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		String localizacion = "Calle Callejero";
+		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
+
+		Incidencia incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, tipo);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, TipoIncidencia.Servicios);
+		vecino = new Ciudadano("Jose", "66666666F", "Calle callejero", "666444555");
+		incidencia = gi.presentarIncidencia(vecino, "Otra Calle", descripcion, TipoIncidencia.Transportes);
+		
+		// Act
+		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
+		// Assert
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro fechaInicio distinto de tipo Date");
+	}
+	
+	@DisplayName("CP08-Prueba1.1_buscarIncidencias no válido con fechaFin distintia de tipo Date")
+	@Test
+	void CP08_Prueba1_1_buscarIncidencias() {
+		// Arrange
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni = "12345678P";
+		String idIncidencia = "I1";
+		Date fechaInicio = new Date(2021 - 1900, 01, 01);
+		Date fechaFin = new Date("mala");
+		TipoIncidencia tipo = TipoIncidencia.Desperfectos;
+
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		String localizacion = "Calle Callejero";
+		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
+
+		Incidencia incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, tipo);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, TipoIncidencia.Servicios);
+		vecino = new Ciudadano("Jose", "66666666F", "Calle callejero", "666444555");
+		incidencia = gi.presentarIncidencia(vecino, "Otra Calle", descripcion, TipoIncidencia.Transportes);
+		
+		// Act
+		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
+		// Assert
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro fechaFin distinto de tipo Date");
+	}
+	
+	@DisplayName("CP09-Prueba1.1_buscarIncidencias no válido con fechaIni > fechaFin")
+	@Test
+	void CP09_Prueba1_1_buscarIncidencias() {
+		// Arrange
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni = "12345678P";
+		String idIncidencia = "I1";
+		Date fechaInicio = new Date(2021 - 1900, 12, 31); 
+		Date fechaFin = new Date(2021 - 1900, 01, 01);
+		TipoIncidencia tipo = TipoIncidencia.Desperfectos;
+
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		String localizacion = "Calle Callejero";
+		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
+
+		Incidencia incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, tipo);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, TipoIncidencia.Servicios);
+		vecino = new Ciudadano("Jose", "66666666F", "Calle callejero", "666444555");
+		incidencia = gi.presentarIncidencia(vecino, "Otra Calle", descripcion, TipoIncidencia.Transportes);
+		
+		// Act
+		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
+		// Assert
+		assertNull(resultadoObtenido, "Prueba fallida, permite fechaInicio > fechaFin");
+	}
+	
+	@DisplayName("CP10-Prueba1.1_buscarIncidencias no válido con parámetro tipo distinto de tipoIncidencia")
+	@Test
+	void CP10_Prueba1_1_buscarIncidencias() {
+		// Arrange
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni = "12345678P";
+		String idIncidencia = "I1";
+		Date fechaInicio = new Date(2021 - 1900, 12, 31); 
+		Date fechaFin = new Date(2021 - 1900, 01, 01);
+		TipoIncidencia tipo = TipoIncidencia.valueOf("Otra");
+
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		String localizacion = "Calle Callejero";
+		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
+
+		Incidencia incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, tipo);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, TipoIncidencia.Servicios);
+		vecino = new Ciudadano("Jose", "66666666F", "Calle callejero", "666444555");
+		incidencia = gi.presentarIncidencia(vecino, "Otra Calle", descripcion, TipoIncidencia.Transportes);
+		
+		// Act
+		ArrayList<Incidencia> resultadoObtenido = gi.buscarIncidencias(dni, idIncidencia,fechaInicio, fechaFin, tipo);
+		// Assert
+		assertNull(resultadoObtenido, "Prueba fallida, permite parámetro parámetro tipo distinto de tipoIncidencia");
+	}
+	
+	
+	@DisplayName("CP01-Prueba3.1_vincularIncidencia válido que incluye todas las clases válidas")
+	@Test
+	void CP1_Prueba3_3_vincularIncidencia() {
+		//Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		
+		String localizacion = "Calle Callejero";
+		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
+
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, localizacion, descripcion, TipoIncidencia.Desperfectos);
+		ArrayList<Incidencia> incidencias = gi.incidenciasSinProceso();
+		
+		Concejal responsable=new Concejal("Javier", "45959101H", "Santiago", "666666666");
+		Proceso proceso = gp.crearNuevoProceso("Cambiar bombillas", responsable, "Hay que cambiar las bombillas", null);
+		
+		//Act
+		gp.vincularIncidencia(proceso, incidencias);
+		//Assert
+				assertAll(
+						() -> {assertTrue(!proceso.getIncidencias().isEmpty(),"Prueba fallida, no hay ninguna incidencia asignada al proceso");},
+						() -> {assertNotNull(incidencia.getProceso(), "Prueba fallida, la incidencia no tiene ningún proceso asignado");},
+						() -> {assertEquals(proceso, incidencia.getProceso(), "Prueba fallida, la incidencia no está asignada al proceso indicado");}
+						);
 	}
 	
 	
