e2355aae250215df42c616bb250bddf3d36c92ae
AlbertoFontanUSC <alberto.fontan@outlook.es>
2021-05-15 22:03:40 +0200

    TestIncidencias

--- PrjGrp2A/test/incidencias/TestGestionIncidencias.java
+++ PrjGrp2A/test/incidencias/TestGestionIncidencias.java
@@ -70,7 +70,7 @@ class TestGestionIncidencias {
 		// Assert
 		Object[] esperado = resultadoEsperado.toArray();
 		Object[] obtenido = resultadoObtenido.toArray();
-		assertArrayEquals(esperado, obtenido, "Búsqueda incorrecta con parámetros válidos");
+		assertArrayEquals(esperado, obtenido, "Resúltado de búsqueda incorrecto con parámetros válidos");
 	}
 
 	@DisplayName("CP02-Prueba1.1_buscarIncidencias no válido con parámetro dni incorrecto")
@@ -345,7 +345,7 @@ class TestGestionIncidencias {
 			assertNotNull(incidencia.getProceso(), "Prueba fallida, la incidencia no tiene ningún proceso asignado");
 		}, () -> {
 			assertEquals(proceso, incidencia.getProceso(),
-					"Prueba fallida, la incidencia no está asignada al proceso indicado");
+					"Prueba fallida, no se ha asignado al proceso la incidencia correcta");
 		});
 	}
 
@@ -364,9 +364,7 @@ class TestGestionIncidencias {
 		ArrayList<Incidencia> incidencias = gi.incidenciasSinProceso();
 
 		Concejal responsable = new Concejal("Javier", "45959101H", "Santiago", "666666666");
-		Proceso proceso = gp.crearNuevoProceso(null, null, "Hay que cambiar las bombillas",
-				new ArrayList<Incidencia>());
-
+		Proceso proceso = new Proceso("Proceso Incorrecto", null, null);
 		// Act
 		gp.vincularIncidencia(proceso, incidencias);
 		// Assert
@@ -380,15 +378,11 @@ class TestGestionIncidencias {
 		GestionProcesos gp = new GestionProcesos();
 		GestionIncidencias gi = new GestionIncidencias();
 
-		String localizacion = "Calle Callejero";
-		String descripcion = "Descripcion de prueba para la creacion de una incidencia de prueba";
 
-		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
-		Incidencia incidencia = gi.presentarIncidencia(null, localizacion, descripcion, TipoIncidencia.Desperfectos);
 		ArrayList<Incidencia> incidencias = new ArrayList<>();
-		incidencias.add(incidencia);
-
+		incidencias.add(new Incidencia(null, "Incidencia Incorrecta", null));
 		Concejal responsable = new Concejal("Javier", "45959101H", "Santiago", "666666666");
+		
 		Proceso proceso = gp.crearNuevoProceso("Cambiar bombillas", responsable, "Hay que cambiar las bombillas",
 				new ArrayList<Incidencia>());
 		ArrayList<Incidencia> incidenciasAntes = proceso.getIncidencias();
@@ -397,7 +391,7 @@ class TestGestionIncidencias {
 		ArrayList<Incidencia> incidenciasDespues = proceso.getIncidencias();
 		// Assert
 		assertEquals(incidenciasAntes, incidenciasDespues,
-				"Prueba fallida, se han modificado las incidencias asignadas al prceso despues de vincular incidencias incorrectas");
+				"Prueba fallida, se han modificado las incidencias asignadas al proceso al vincular incidencias incorrectas");
 	}
 
 	@DisplayName("CP04-Prueba3.1_vincularIncidencia válido donde el primer y el último elemento del parámetro incidencias son correctos")
@@ -472,10 +466,8 @@ class TestGestionIncidencias {
 		gp.vincularIncidencia(proceso, incidencias);
 		ArrayList<Incidencia> obtenido = proceso.getIncidencias();
 		// Assert
-		assertAll(
-			() -> {assertFalse(obtenido.contains(incidencias.get(0)),"Prueba fallida, se ha vinculado la primera incidencia como incorrecta");}, 
-			() -> {assertEquals(esperado, obtenido, "Prueba fallida, se han vinculado al proceso incidencias incorrectas");
-		});	
+		assertEquals(esperado, obtenido, "Prueba fallida, se han vinculado al proceso incidencias incorrectas");
+	
 	}
 	
 	@DisplayName("CP06-Prueba3.1_vincularIncidencia no válido con parámetro incidencias vacío")
@@ -538,10 +530,9 @@ class TestGestionIncidencias {
 		);
 	}
 	
-	@DisplayName("CP08-Prueba3.1_vincularIncidencia válido, parámetro incidencias contiene incidencia ya asginada")
+	@DisplayName("CP08-Prueba3.1_vincularIncidencia no válido, parámetro incidencias contiene incidencia ya asginada")
 	@Test
 	void CP08_Prueba3_3_vincularIncidencia() {
-		/*CAMBIAR NO DOC A VÁLIDO PARA QUE SI SE VINCULA UNHA INCIDENCIA XA VINCULADA SE REASIGNE*/
 		// Arrange
 		GestionProcesos gp = new GestionProcesos();
 		GestionIncidencias gi = new GestionIncidencias();
@@ -566,11 +557,262 @@ class TestGestionIncidencias {
 		gp.vincularIncidencia(procesoNuevo, incidencias);
 		// Assert
 		assertAll(
-				() -> {assertEquals(procesoNuevo, incidencia.getProceso(), "Prueba fallida, no se ha resignado el proceso");},
-				() -> {assertFalse(procesoAntiguo.getIncidencias().contains(incidencia), "Prueba fallida, el antiguo proceso contnúa con la incidencia asignada");},
-				() -> {assertEquals(incidencias, procesoNuevo.getIncidencias(), "Prueba fallida, al nuevo proceso no se la asignado la incidencia");}
+				() -> {assertNotEquals(procesoAntiguo.getIncidencias(), procesoNuevo.getIncidencias(), "Prueba fallida, las incidencias están vinculadas a más de un proceso");},
+				() -> {assertNotEquals(procesoNuevo, incidencia.getProceso(), "Prueba fallida, se ha asignado la incidencia al nuevo proceso");}
 		);
 	}
+	
+	
+	@DisplayName("CP01-Prueba8.1_buscarIncidencias lista de incidencias almacenadas en el sistema vacía")
+	@Test
+	void CP01_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		ArrayList<Incidencia> obtenido = new ArrayList<>();
+		//Act
+		obtenido = gi.buscarIncidencias(null, null, null, null, null);
+		//Assert
+		assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP02-Prueba8.1_buscarIncidencias parámetro fechaInicio != null y fechaInicio > fechaInicioIncidencia")
+	@Test
+	void CP02_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni ="12345678P";
+		Ciudadano vecino = new Ciudadano("Raul", dni, "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2021 - 1900, 12, 31);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(dni, "I1", fechaIni, fechaFin, TipoIncidencia.Desperfectos);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP03-Prueba8.1_buscarIncidencias parámetro fechaInicio != null y fechaInicioInicidencia > fechaFin")
+	@Test
+	void CP03_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni ="12345678P";
+		Ciudadano vecino = new Ciudadano("Raul", dni, "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2020 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(dni, "I1", fechaIni, fechaFin, TipoIncidencia.Desperfectos);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP04-Prueba8.1_buscarIncidencias con parámetros dni, idIncidencia y tipo distintos del dni, idIncidencia y tipo de las incidencias existentes en el sistema")
+	@Test
+	void CP04_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias("12345678A" , "I2", fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP05-Prueba8.1_buscarIncidencias con parámetros dni == null, id distinto y tipo distinto")
+	@Test
+	void CP05_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(null , "I2", fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP06-Prueba8.1_buscarIncidencias con parámetros dni != null e igual al de la incidencia existente")
+	@Test
+	void CP06_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		String dni= "12345678P";
+		Ciudadano vecino = new Ciudadano("Raul", dni, "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		ArrayList<Incidencia> esperado = new ArrayList<>();
+		esperado.add(incidencia);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, "Calle callejerao" , "Descripcion", TipoIncidencia.Servicios);
+		
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(dni , "I2", fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertAll(
+				 () -> {assertFalse(obtenido.isEmpty(), "Prueba fallida, no ha encontrado incidencias");},
+				 () -> {assertEquals(obtenido, esperado, "Prueba fallida, no se ha obtenido la incidencia esperada");}
+				 );
+	}
+	
+	@DisplayName("CP07-Prueba8.1_buscarIncidencias con parámetros dni distinto, id == null y tipo distinto")
+	@Test
+	void CP07_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias("12345678A" , null, fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP08-Prueba8.1_buscarIncidencias con parámetros idIncidencia != null e igual al de la incidencia existente")
+	@Test
+	void CP08_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		ArrayList<Incidencia> esperado = new ArrayList<>();
+		esperado.add(incidencia);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, "Calle callejerao" , "Descripcion", TipoIncidencia.Servicios);
+		
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias("12345678A" , "I1", fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertAll(
+				 () -> {assertFalse(obtenido.isEmpty(), "Prueba fallida, no ha encontrado incidencias");},
+				 () -> {assertEquals(obtenido, esperado, "Prueba fallida, no se ha obtenido la incidencia esperada");}
+				 );
+	}
+	
+	@DisplayName("CP09-Prueba8.1_buscarIncidencias con parámetros dni == null e id igual a id de la incidencia existente")
+	@Test
+	void CP09_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		ArrayList<Incidencia> esperado = new ArrayList<>();
+		esperado.add(incidencia);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, "Calle callejerao" , "Descripcion", TipoIncidencia.Servicios);
+		
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(null , "I1", fechaIni, fechaFin, TipoIncidencia.Transportes);
+		//Assert
+		 assertAll(
+				 () -> {assertFalse(obtenido.isEmpty(), "Prueba fallida, no ha encontrado incidencias");},
+				 () -> {assertEquals(obtenido, esperado, "Prueba fallida, no se ha obtenido la incidencia esperada");}
+				 );
+	}
+	
+	@DisplayName("CP10-Prueba8.1_buscarIncidencias con parámetros dni, idIncidencia distintos y tipo == null")
+	@Test
+	void CP10_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias("12345678A" , "I2", fechaIni, fechaFin, null);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP11-Prueba8.1_buscarIncidencias con parámetros dni, idIncidencia distintos y tipo igual al de la incidencia")
+	@Test
+	void CP11_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias("12345678A" , "I2", fechaIni, fechaFin, TipoIncidencia.Desperfectos);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP12-Prueba8.1_buscarIncidencias con parámetros dni == null, id distinto y tipo igual al de la incidencia existente")
+	@Test
+	void CP12_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(null , "I2", fechaIni, fechaFin, TipoIncidencia.Desperfectos);
+		//Assert
+		 assertTrue(obtenido.isEmpty(), "Prueba fallida, ha encontrado incidencias");
+	}
+	
+	@DisplayName("CP13-Prueba8.1_buscarIncidencias con parámetros dni == null, id == null y tipo igual al de la incidencia existente")
+	@Test
+	void CP13_Prueba8_1_buscarIncidencias() {
+		// Arrange
+		GestionProcesos gp = new GestionProcesos();
+		GestionIncidencias gi = new GestionIncidencias();
+		Ciudadano vecino = new Ciudadano("Raul", "12345678P", "Calle callejero", "666666666");
+		Incidencia incidencia = gi.presentarIncidencia(vecino, "Calle Callejero","Descripcion", TipoIncidencia.Desperfectos);
+		Date fechaIni = new Date(2020 - 1900, 01, 01);
+		Date fechaFin = new Date(2022 - 1900, 12, 31);
+		ArrayList<Incidencia> esperado = new ArrayList<>();
+		esperado.add(incidencia);
+		vecino = new Ciudadano("Pepe", "77777777Z", "Calle callejero", "666555555");
+		incidencia = gi.presentarIncidencia(vecino, "Calle callejerao" , "Descripcion", TipoIncidencia.Servicios);
+		
+		//Act
+		 ArrayList<Incidencia> obtenido = gi.buscarIncidencias(null , null, fechaIni, fechaFin, TipoIncidencia.Desperfectos);
+		//Assert
+		//Assert
+		 assertAll(
+				 () -> {assertFalse(obtenido.isEmpty(), "Prueba fallida, no ha encontrado incidencias");},
+				 () -> {assertEquals(obtenido, esperado, "Prueba fallida, no se ha obtenido la incidencia esperada");}
+				 );
+	}
+	
 	
 
 }
